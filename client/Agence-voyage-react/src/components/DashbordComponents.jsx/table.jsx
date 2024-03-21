89b46950b6a73bc68059b3eadab227743e591a27
mariem kamaych <kamaychmeryem@gmail.com>
2024-03-21 14:29:08 +0000

    delete package

--- client/Agence-voyage-react/src/components/DashbordComponents.jsx/table.jsx
+++ client/Agence-voyage-react/src/components/DashbordComponents.jsx/table.jsx
@@ -10,14 +10,28 @@ const table = () => {
   const display = useSelector((state) => state.package.display);
   const { value: allPackages, message } = useSelector((state) => state.package);
   const [addPackage, setAddPackage] = useState(true);
+  const [delete1, setDelete] = useState(true);
+
+  const deletePackage1 = async (id) => {
+    console.log(id);
+    try {
+      await axios.delete(
+        `http://localhost:4000/api/package/deletePackage/${id}`,
+        { withCredentials: true }
+      );
+
+      setDelete(!delete1);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   useEffect(() => {
     dispatch(showPackages());
-  }, [display]);
+  }, [display, delete1]);
 
   const getPackageId = (id) => {
-    
-    navigate('/dashboard/edit/'+id)
+    navigate("/dashboard/edit/" + id);
   };
 
   return (
@@ -207,6 +221,14 @@ const table = () => {
                         <path d="M21.731 2.269a2.625 2.625 0 00-3.712 0l-1.157 1.157 3.712 3.712 1.157-1.157a2.625 2.625 0 000-3.712zM19.513 8.199l-3.712-3.712-12.15 12.15a5.25 5.25 0 00-1.32 2.214l-.8 2.685a.75.75 0 00.933.933l2.685-.8a5.25 5.25 0 002.214-1.32L19.513 8.2z"></path>
                       </svg>
                     </span>
+                  </button>
+                  <button
+                    className="bg-red-600 px-2 text-white rounded"
+                    onClick={() => {
+                      deletePackage1(singlepackage?._id);
+                    }}
+                  >
+                    delete
                   </button>
                 </td>
               </tr>
